0841a40cdc37ead7ebb197cdd7e5073eda3a0846
rednikeeg <maks.schoolforvv@gmail.com>
2021-09-05 16:13:13 +0300

    Entity interface

--- src/main/java/entities/Entity.java
+++ src/main/java/entities/Entity.java
@@ -1,4 +1,14 @@
 package entities;
 
+import java.sql.ResultSet;
+
 public interface Entity {
+    // Returns names of fields should be inserted
+    String getFieldNames();
+    // Returns values of fields should be inserted
+    String getFieldValues();
+    // Returns printable information about entity for output
+    String getPrintable();
+    // Applies values from ResultSet to entity
+    void apply(ResultSet rs);
 }
